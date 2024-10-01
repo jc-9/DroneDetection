b596f83de06acddc1b73e86b2a540c0cde1bd681
jc-9 <justinmelmarclay@gmail.com>
2024-10-01 14:14:46 -0500

    Crosshairs with sublines

--- yolov8_webcam_inference.py
+++ yolov8_webcam_inference.py
@@ -1,54 +1,12 @@
 """
 Justin Clay
-Run Yolo inference with local Webcam stream using traditional webcam and not a VPU Webcam (i.e. OAK-D, Luxonis)
+ACD Drone Detection Demo
 """
 
 from ultralytics import YOLO
 import cv2
-import os.path
 import numpy as np
 import glob
-import torch
-import datetime
-
-
-# def capture_image(save_path):
-#     cap = cv2.VideoCapture(0)
-#     if not os.path.exists(save_path):
-#         os.makedirs(save_path)
-#     if not cap.isOpened():
-#         print("Error: Could not open webcam.")
-#         return
-#     img_number = 0
-#     while True:
-#         # Capture frame-by-frame
-#         ret, frame = cap.read()
-#
-#         if not ret:
-#             print("Error: Failed to capture image.")
-#             break
-#
-#         # Display the resulting frame
-#         cv2.imshow('Press "c" to capture image, "q" to quit', frame)
-#
-#         # Wait for a key press
-#         key = cv2.waitKey(1) & 0xFF
-#
-#         if key == ord('c'):  # 'c' to capture the image
-#             # Save the captured image
-#             filename = f'calibrationImage_{img_number}.jpg'
-#             path = os.path.join(save_path, filename)
-#             img_number += 1
-#             cv2.imwrite(path, frame)
-#             print(f"Image saved to {save_path}")
-#             # break
-#         elif key == ord('q'):  # 'q' to quit without capturing
-#             print("Quit without capturing image.")
-#             break
-#
-#     # When everything is done, release the capture and close windows
-#     cap.release()
-#     cv2.destroyAllWindows()
 
 
 def calibrate_images(camnum):
@@ -106,68 +64,7 @@ def is_defined(var_name):
         return False
 
 
-def single_draw_box(results):
-    """
-    :param yolo resutls object
-    :param detect_class - the class number of the pick location
-    :return: annotated image
-    """
-    global box
-
-    box_colors = {"Green": (0, 255, 0), "Blue": (255, 0, 0), "Red": (0, 0, 255)}
-    boxes_pick_class = results[0].boxes
-    frame = results[0].orig_img
-
-    for obj in enumerate(results[0].boxes):
-        cls = obj[1].cls
-        x1, y1, x2, y2 = obj[1].xyxy[0].tolist()
-        if cls == 1:
-            cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), box_colors['Green'], thickness=3)
-            text = "SCREW_PRESENT"
-            font = cv2.FONT_HERSHEY_SIMPLEX
-            font_scale = 0.5
-            thickness = 2
-            # text_color = (255, 255, 255)  # White text
-            # bg_color = (0, 0, 255)  # Red background
-
-            # Get the size of the text box
-            (text_width, text_height), baseline = cv2.getTextSize(text, font, font_scale, thickness)
-            cv2.rectangle(frame, (int(x1), int(y1)-30), (int(x1)+text_width, int(y1)+text_height-10), box_colors['Green'], -1)
-            cv2.putText(frame, f'SCEW_COMPLETE', (int(x1), int(y1) - 10), cv2.FONT_HERSHEY_SIMPLEX, font_scale, color=(0, 0, 0), thickness=thickness)
-
-        else:
-            cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), box_colors['Red'], thickness=3)
-            text = "SCREW_PRESENT"
-            font = cv2.FONT_HERSHEY_SIMPLEX
-            font_scale = 0.5
-            thickness = 2
-            # text_color = (255, 255, 255)  # White text
-            # bg_color = (0, 0, 255)  # Red background
-
-            # Get the size of the text box
-            (text_width, text_height), baseline = cv2.getTextSize(text, font, font_scale, thickness)
-            cv2.rectangle(frame, (int(x1), int(y1) - 30), (int(x1) + text_width, int(y1) + text_height - 10),
-                          box_colors['Red'], -1)
-            cv2.putText(frame, f'SCEW_MISSING', (int(x1), int(y1) - 10), cv2.FONT_HERSHEY_SIMPLEX, font_scale,
-                        color=(255, 255, 255), thickness=thickness)
-
-    # for box in boxes_pick_class:
-    #     x1, y1, x2, y2 = box.xyxy[0].tolist()
-    #     cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), box_colors['Green'], thickness=3)
-        # cv2.putText(frame, f'PICK THIS', (int(x1), int(y1) - 10),
-        #             cv2.FONT_HERSHEY_SIMPLEX, 0.5, color=(0, 255, 0), thickness=2)
-
-    return frame
-
-
-def yolo_to_crosshair(img, bbox, sub_line_length=10):
-    """
-    Convert YOLO bounding box format to a crosshair shape on the image.
-
-    Parameters:
-    img: The input image (numpy array).
-    bbox: The bounding box in YOLO format [x_center, y_center, width, height] (relative to image size).
-    """
+def crosshair(img, bbox, sub_line_length=10, sub_line_offset = 30):
     img_height, img_width = img.shape[:2]
     for box in bbox:
         # Extract YOLO format bounding box
@@ -176,8 +73,7 @@ def yolo_to_crosshair(img, bbox, sub_line_length=10):
         # Convert YOLO format to pixel coordinates
         x_center = int(x_center)
         y_center = int(y_center)
-        # box_width = int(width_rel * img_width)
-        # box_height = int(height_rel * img_height)
+
 
         # Calculate bounding box corners
         x_min = int(x_center - box_width // 2)
@@ -190,72 +86,34 @@ def yolo_to_crosshair(img, bbox, sub_line_length=10):
 
         # Draw horizontal line (crosshair)
         cv2.line(img, (x_min, y_center), (x_max, y_center), (0, 0, 255), 2)
-        # Vertical sub-crosshairs
-        cv2.line(img, (x_center - sub_line_length, y_min), (x_center + sub_line_length, y_min), (0, 255, 0), 2)  # Top
-        cv2.line(img, (x_center - sub_line_length, y_max), (x_center + sub_line_length, y_max), (0, 255, 0),
-                 2)  # Bottom
 
-        # Horizontal sub-crosshairs
-        cv2.line(img, (x_min, y_center - sub_line_length), (x_min, y_center + sub_line_length), (0, 255, 0), 2)  # Left
-        cv2.line(img, (x_max, y_center - sub_line_length), (x_max, y_center + sub_line_length), (0, 255, 0), 2)  # Right
+        # Vertical sub-crosshairs (closer to the center)
+        cv2.line(img, (x_center - sub_line_length, y_center - sub_line_offset),
+                 (x_center + sub_line_length, y_center - sub_line_offset), (0, 255, 0), 2)  # Above center
+        cv2.line(img, (x_center - sub_line_length, y_center + sub_line_offset),
+                 (x_center + sub_line_length, y_center + sub_line_offset), (0, 255, 0), 2)  # Below center
+
+        # Horizontal sub-crosshairs (closer to the center)
+        cv2.line(img, (x_center - sub_line_offset, y_center - sub_line_length),
+                 (x_center - sub_line_offset, y_center + sub_line_length), (0, 255, 0), 2)  # Left of center
+        cv2.line(img, (x_center + sub_line_offset, y_center - sub_line_length),
+                 (x_center + sub_line_offset, y_center + sub_line_length), (0, 255, 0), 2)  # Right of center
 
     return img
 
 
-def main(camnum, weights_path: str, write_flag: bool):
+def main(camnum, weights_path: str):
     global results
-    calibrate_camera = False
-    undistort_image = False
-    if calibrate_camera:
-        ret, mtx, dist, rvecs, tvecs = calibrate_images(camnum)
-        calibrate_camera = False  # Dont Change this value
-        undistort_image = False  # Dont Change this value
-
     model = YOLO(weights_path)
-    # frame_width = 480
-    # frame_height = 480
-    flag1 = False
-    write_video = write_flag
-    exp_name = 'ALEXA_DEMO'
-    current_date = datetime.datetime.now()
-
     cap = cv2.VideoCapture(camnum)
 
     # Loop through the video frames
     while cap.isOpened():
         # Read a frame from the video
         success, frame = cap.read()
-        # Write the frame to the video file
-        # if flag1 and write_video:
-        #     frame_width = int(frame.shape[1])
-        #     frame_height = int(frame.shape[0])
-        #     fourcc = cv2.VideoWriter_fourcc(*'XVID')
-        #     out = cv2.VideoWriter('output.avi', fourcc, 20.0, (frame_width, frame_height))
-        #     directory = f'/Volumes/PycharmProjects/Assembly_Assist_Project/data/1.RawData/{exp_name}'
-        #     if not os.path.exists(directory):
-        #         # Create the directory
-        #         os.makedirs(directory)
-        #         print(f'Directory "{directory}" created.')
-        #     else:
-        #         print(f'Directory "{directory}" already exists.')
-        #     file = f'{exp_name}_{current_date}.mp4'
-        #     fullpath = os.path.join(directory, file)
-        #     outmp4 = cv2.VideoWriter(fullpath, cv2.VideoWriter_fourcc(*'XVID'), 10, (frame_width, frame_height))
-        #     flag1 = False
-        #
-        # if success:
-        #     if undistort_image:
-        #         h, w = frame.shape[:2]
-        #         newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w, h), 1, (w, h))
-        #         dst = cv2.undistort(frame, mtx, dist, None, newcameramtx)
-        #         x, y, w, h = roi
-        #         frame = dst[y:y + h, x:x + w]
         results = model(frame, conf=0.6, verbose=False)
-        annotated_frame = yolo_to_crosshair(frame,results[0].boxes)
-        # annotated_frame = results[0].plot()
+        annotated_frame = crosshair(frame, results[0].boxes)
         cv2.imshow('Webcam Object Detection', annotated_frame)
-        # if write_video:
-        #     outmp4.write(annotated_frame)
         if cv2.waitKey(1) == ord('q'):
             break
 
@@ -264,7 +122,6 @@ def main(camnum, weights_path: str, write_flag: bool):
 
 
 if __name__ == '__main__':
-    cameraNumber = '/home/justin/Videos/Screencasts/Screencast from 2024-10-01 14-04-50.webm'
+    cameraNumber = '/home/justin/kalvoai/drone_flying_test_video.m4v'
     weights_path = '/home/justin/PycharmProjects/DroneDetection/data/drone_test_results_09_30_242/weights/best.pt'
-    write_video_flag = False
-    main(cameraNumber, weights_path, write_video_flag)
+    main(cameraNumber, weights_path)
